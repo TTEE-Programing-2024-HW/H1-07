7b929eeed33b129fccdf014535ecc7f2a4f23da9
游喬亘 <u11219007@o365.ttu.edu.tw>
2024-05-06 18:54:01 +0800

    請使用輸入4個數字的密碼將密碼值預設為2024。若輸入正確，則出現主選單

--- H1.07-1.c
+++ H1.07-1.c
@@ -3,6 +3,8 @@
 
 int main(void) //主函數main()從這開始 
 {
+	int password=2024,input,a=3,n;//宣告整數變數passwrd,input，將password的值設為2024 
+	char ch,ans;
     printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串          
     printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串
     printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串
@@ -31,4 +33,24 @@ int main(void) //主函數main()從這開始
     
     system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
 	system("CLS");//清除螢幕
+	printf("請輸入4個數字的密碼： ");//提示使用者輸入4個數字的密碼
+    scanf("%d", &input); //從鍵盤讀入數字
+while(1){
+
+    if (input == password) //若輸入正確
+    {
+    	
+	
+    printf("*~*~*~*~*~*~*~*~*~*~*~*~*~*\n"); //印出字串          
+    printf("| a. 畫出直角三角形       |\n"); //印出字串
+    printf("| b. 顯示乘法表           |\n"); //印出字串
+    printf("| c. 結束                 |\n"); //印出字串
+    printf("*~*~*~*~*~*~*~*~*~*~*~*~*~*\n"); //印出字串
+    
+    system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕
+	
+
+} 
+} 
 } 
