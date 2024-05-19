8583ad67bf1efef21207e13accaab0558d80fab9
Chiao-xuan. <u11219007@o365.ttu.edu.tw>
2024-05-19 17:41:09 +0800

    要求使用者輸入4個數字的密碼。若輸入正確，則出現歡迎!並且輸出表格

--- HW3.c
+++ HW3.c
@@ -1,4 +1,5 @@
-lude <stdlib.h>//把stdlib.h這個檔案含括進來 
+#include <stdio.h>//把stdio.h這個檔案含括進來  
+#include <stdlib.h>//把stdlib.h這個檔案含括進來  
 
 int main(void) //主函數main()從這開始 
 {
@@ -32,4 +33,27 @@ int main(void) //主函數main()從這開始
     
     system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
 	system("CLS");//清除螢幕
+	printf("請輸入4個數字的密碼： ");//提示使用者輸入4個數字的密碼
+    scanf("%d", &input); //從鍵盤讀入數字
+while(1){
+
+    if (input == password) //若輸入正確
+    {
+    	
+	printf("歡迎!\n");
+	system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕
+	
+    printf(" *-*-*-*-*-*-[Booking System]*-*-*-*-*-*-\n"); //印出字串          
+    printf(" |  a. Available seats                  |\n"); //印出字串
+    printf(" |  b. Arrange for you                  |\n"); //印出字串
+    printf(" |  c. Choose by yourself               |\n"); //印出字串
+    printf(" |  d. Exit                             |\n"); //印出字串
+    printf(" ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*\n"); //印出字串
+    system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕
+	
 }
+}
+}
+
