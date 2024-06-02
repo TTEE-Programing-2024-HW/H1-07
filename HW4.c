365dee8ef67b10fed84815dc358dc51af8b0d09a
游喬亘 <u11219007@o365.ttu.edu.tw>
2024-06-02 09:06:12 +0800

    展示個人風格的畫面

---
+++ HW4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>//把stdio.h這個檔案含括進來  
+#include <stdlib.h>//把stdlib.h這個檔案含括進來 
+
+int main(void) //主函數main()從這開始 
+{
+	int password=2024,input,a=3,n;//宣告整數變數passwrd,input，將password的值設為2024 
+	char ch,ans;
+    printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串          
+    printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111        77777777777777777777777777777777\n"); //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                              7777777777\n"); //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                             7777777777\n");  //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                            7777777777\n");   //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                           7777777777\n");    //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111   0000000000             7777777777\n");     //印出字串
+    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH        11111111   0000000000            7777777777\n");      //印出字串
+    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH        11111111   0000000000           7777777777\n");       //印出字串
+    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH        11111111   0000000000          7777777777\n");        //印出字串
+    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH        11111111   0000000000         7777777777\n");         //印出字串
+    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH        11111111                     7777777777\n");          //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                    7777777777\n");           //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                   7777777777\n");            //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                  7777777777\n");             //印出字串 
+    printf("HHHHHHHH                HHHHHHHH        11111111                 7777777777\n");              //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111                7777777777\n");               //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111               7777777777\n");                //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111              7777777777\n");                 //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111             7777777777\n");                  //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111            7777777777\n");                   //印出字串
+    printf("HHHHHHHH                HHHHHHHH        11111111           7777777777\n");                    //印出字串
+    
+    system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕
+
+} 
