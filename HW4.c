fe887511069761e0260fb98379935c2bf0fe8cb1
游喬亘 <u11219007@o365.ttu.edu.tw>
2024-06-02 09:32:12 +0800

    請使用者輸入4個數字的密碼，密碼預設值為2024。若輸入正確，出現歡迎!並且出現目錄；若輸入連續三次錯誤的密碼，就顯示警告訊息並且結束程式。

--- HW4.c
+++ HW4.c
@@ -33,5 +33,45 @@ int main(void) //主函數main()從這開始
     
     system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
 	system("CLS");//清除螢幕
+	
+    printf("請輸入4個數字的密碼： ");//提示使用者輸入4個數字的密碼
+    scanf("%d", &input); //從鍵盤讀入數字
 
+while(1){
+
+    if (input == password) //若輸入正確
+    { 
+    printf("歡迎!\n");
+	system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕
+	
+	printf("------------[Grade System]----------\n"); 
+    printf("|  a. Enter student grades         |\n");
+    printf("|  b. Display student grades       |\n");
+    printf("|  c. Search for student grades    |\n");
+    printf("|  d. Grade ranking                |\n");    
+    printf("|  e. Exit system                  |\n");   
+    printf("------------------------------------\n");
+    
+	system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕     
+    break;     
 } 
+
+else if(input!=password&&a>1)//若是錯誤，
+	{
+		a=a-1;
+        printf("錯誤!,還剩%d機會",a);
+        fflush(stdin);//使input buffer淨空 
+        scanf("%d",&input);
+    }
+    else
+	{
+    system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");//清除螢幕
+	return 0;
+	}
+} 
+    system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵
+	return 0;
+}
